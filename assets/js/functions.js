b6c2ea406836464af1dc7ae33897d6ca3551427e
Leshy <igroeneveld@gmail.com>
2020-05-10 15:39:46 +0200

    Add files via upload

---
+++ assets/js/functions.js
@@ -0,0 +1,26 @@
+/* JavaScript for Base Apparel Coming Soon challenge from Frontend Mentor */
+
+/* Validate the email address (regex grabbed from emailregex.com) */
+function validateInput(){
+  var email = document.getElementById("input-email").value;
+  var regex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  if (!email) setState("error");
+  else {
+    var emailStatus = regex.test(String(email).toLowerCase());
+    if (emailStatus) setState("success");
+    else setState("error");
+  }
+}
+
+/* Set error or success state */
+function setState(state){
+  var input = document.getElementById("input-email");
+      input.classList.remove("input--error","input--success");
+      input.classList.add("input--" +state);
+      input.parentElement.classList.remove("label--error","label--success");
+      input.parentElement.classList.add("label--" +state);
+}
+
+/* Put an event handler on the button */
+var submit = document.getElementById("button-submit");
+submit.addEventListener("click", validateInput);
